9e16570673f415a4e9c6c994a0efeb2be86a5b26
IrinaDedekina <dedekina92@yandex.ru>
2020-12-25 19:32:51 +0300

    Views refactoring

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -14,3 +14,15 @@ require("channels")
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
+
+document.addEventListener('DOMContentLoaded', () => {
+  const askButton = document.getElementById('ask-button')
+  const askForm = document.getElementById('ask-form')
+  askButton.addEventListener('click', (event) => {
+    event.preventDefault()
+    setTimeout(() => {
+      let displayStyle = askForm.style.display === 'block' ? 'none' : 'block'
+      askForm.style.display = displayStyle
+    }, 300);
+  })
+})
