36c5022be8592bbcf54d006743ddad426b5182ce
IrinaDedekina <dedekina92@yandex.ru>
2021-01-11 13:19:00 +0300

    users + sessions controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
-  resources :users
-  resources :users
+
+  resources :users, except: [:destroy]
+  resources :sessions, only: [:new, :create, :destroy]
   resources :questions
   root to: "users#index"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  get "show" => "users#show"
+  get "sign_up" => "users#new"
+  get "log_out" => "sessions#destroy"
+  get "log_in" => "sessions#new"
 end
