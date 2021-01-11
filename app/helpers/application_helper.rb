e65a93dc3e57d34b787dc5fe8a9dd270d5ab3c9d
IrinaDedekina <dedekina92@yandex.ru>
2021-01-11 13:58:41 +0300

    questions controller

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,22 +7,7 @@ module ApplicationHelper
     end
   end
 
-  def inclination(number, one, few, many)
-    remainder10 = number % 10
-    remainder100 = number % 100
-
-    if remainder100 >= 11 && remainder100 <= 14
-        return many
-    end
-
-    if remainder10 == 1
-      return one
-    end
-
-    if remainder10 > 4 || remainder10 == 0
-      return many
-    end
-
-    few
+  def fa_icon(icon_class)
+    content_tag "span", "", class: "fa fa-#{icon_class}"
   end
 end
