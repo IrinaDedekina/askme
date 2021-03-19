cca12a48a77e6309a62e2c3c967cbc692d6c5198
IrinaDedekina <dedekina92@yandex.ru>
2021-03-19 11:15:06 +0300

    add Hashtag controller

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   resources :users
   resources :sessions, only: [:new, :create, :destroy]
   resources :questions, except: [:show, :new, :index]
+  resources :hashtags, only: :show, param: :text
   root to: "users#index"
 
   get "sign_up" => "users#new"
