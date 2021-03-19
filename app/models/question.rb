cf0743cdaf5e5f351403a9f8cedf7684de194e85
IrinaDedekina <dedekina92@yandex.ru>
2021-03-19 10:23:44 +0300

    add HashtagQuestion model

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,5 +1,8 @@
 class Question < ApplicationRecord
+  has_many :hashtag_questions, dependent: :destroy
+  has_many :hashtags, through: :hashtag_questions
   belongs_to :user
   belongs_to :author, class_name: "User", optional: true
+
   validates :text, presence: true, length: { maximum: 255 }
 end
